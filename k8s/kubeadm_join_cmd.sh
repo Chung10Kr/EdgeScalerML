86d86c68486f10f2485bb6443523e9166d3893ae
Chung10kr <coohg93@hanmail.net>
2024-11-05 16:28:43 +0900

    - 쿠버네티스 셋팅

--- k8s/kubeadm_join_cmd.sh
+++ k8s/kubeadm_join_cmd.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.64.27:6443 --token uqi7vn.awqnrrnaivjz6396 --discovery-token-ca-cert-hash sha256:a63d08c03d00ffb60e4488957ea28e9d18b1b1dbbe779b85beb3b80d97a3b65f 
+kubeadm join 192.168.64.31:6443 --token knvih3.lj1t8yaq8ux6l70p --discovery-token-ca-cert-hash sha256:5b76cefa48bc470d65abace75ffdebcc6a8c9b9883b905dedaee3cc21df91c44 
